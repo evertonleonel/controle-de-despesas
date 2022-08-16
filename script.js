d14dcaceee117e981a0f91a74b2435abd6fcd7d6
evertonleonel <everton_leonel@hotmail.com>
2022-08-16 17:29:15 -0300

    displays adicionados : balance, despesas e receitas

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const transactionsUl = document.querySelector('#transactions')
+const incomeDisplay = document.querySelector('#money-plus')
+const expenseDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
 
 const dummyTransactions = [
   { id: 1, name: 'Bolo de Chocolate', amount: -20},
@@ -6,7 +9,6 @@ const dummyTransactions = [
   { id: 3, name: 'Torta de Frango', amount: -10},
   { id: 4, name: 'Violão', amount: 150}
 ];
-
 
 const addTransactionsIntoDOM = transaction => {
   const operator = transaction.amount < 0 ? '-' : '+'
@@ -18,8 +20,33 @@ const addTransactionsIntoDOM = transaction => {
   li.innerHTML = `
    ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>
   `
-  console.log(li)
-
+  transactionsUl.append(li)
 }
 
-addTransactionsIntoDOM(dummyTransactions[0])
+const updateBalanceValues = () => {
+  const transactionsAmounts = dummyTransactions
+    .map(transaction => transaction.amount)
+  const total = transactionsAmounts
+    .reduce((accumalator, transaction) => accumalator + transaction, 0)
+    .toFixed(2)
+  const income = transactionsAmounts
+    .filter(value => value > 0)
+    .reduce( (accumalator, value) => accumalator + value, 0)
+    .toFixed(2)
+  const expense = Math.abs(transactionsAmounts
+    .filter(value => value < 0)
+    .reduce((accumalator, value) => accumalator + value , 0))
+    .toFixed(2)
+ 
+  balanceDisplay.textContent = `R$ ${total}`
+  incomeDisplay.textContent = `R$ ${income}`
+  expenseDisplay.textContent = `R$ ${expense}`
+}
+
+//Adicionar transacoes no DOM
+const init = () => {
+  dummyTransactions.forEach(addTransactionsIntoDOM)
+  updateBalanceValues()
+}
+
+init()
