07edf95fc8c3180349d0b4019b7fc3c35ed0545a
evertonleonel <everton_leonel@hotmail.com>
2022-08-16 16:52:12 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,25 @@
+const transactionsUl = document.querySelector('#transactions')
+
+const dummyTransactions = [
+  { id: 1, name: 'Bolo de Chocolate', amount: -20},
+  { id: 2, name: 'Salário', amount: 300},
+  { id: 3, name: 'Torta de Frango', amount: -10},
+  { id: 4, name: 'Violão', amount: 150}
+];
+
+
+const addTransactionsIntoDOM = transaction => {
+  const operator = transaction.amount < 0 ? '-' : '+'
+  const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
+  const amountWithoutOperator = Math.abs(transaction.amount)
+  const li = document.createElement('li');
+  
+  li.classList.add(CSSClass)
+  li.innerHTML = `
+   ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>
+  `
+  console.log(li)
+
+}
+
+addTransactionsIntoDOM(dummyTransactions[0])
